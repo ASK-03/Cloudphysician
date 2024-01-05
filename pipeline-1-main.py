7313cc2056bb712456b524ad5684f6831b29daa3
ASK-03 <abhisheksinghkushwaha22@gmail.com>
2024-01-05 13:24:05 +0530

    Upload required files

--- pipeline-1-main.py
+++ pipeline-1-main.py
@@ -7,6 +7,12 @@ import threading
 import cv2
 from time import perf_counter, sleep
 import os
+import tensorflow as tf
+import numpy as np
+from time import perf_counter
+import easyocr
+from ultralytics import YOLO
+from extract_information import extract_information_from_image
 
 """
 TODO:
@@ -14,9 +20,7 @@ TODO:
         [] - if only one image is given 
         [done] - if we have to do it using feed from webcam
     - the pipeline gets stopped if we get more number of points after segmentation add that.
-
 """
-
 
 def initialize() -> None:
     """
@@ -24,17 +28,19 @@ def initialize() -> None:
     """
     format = "%(asctime)s [%(levelname)s] %(message)s"
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.ERROR,
         format=format,
         handlers=[logging.FileHandler("pipeline-1.log")],
     )
 
-    from ultralytics import YOLO
-
-    logging.log(logging.INFO, "Loading the YOLO model")
-    global model
-    model = YOLO("./model_yolo/best.pt")
-    logging.log(logging.INFO, "Loaded the YOLO model")
+    try:
+        global model
+        model = YOLO(model="./model_yolo/best.pt")
+        logging.log(logging.INFO, "Loaded the YOLO model")
+    except Exception as e:
+        logging.log(logging.ERROR, e)
+        logging.log(logging.ERROR, "Unable to load the YOLO model")
+        exit(1)
 
     logging.log(level=logging.INFO, msg="Creating the directory structure!")
     if not os.path.exists("images"):
@@ -43,6 +49,54 @@ def initialize() -> None:
         capture_img.create_dir(path="processed_images")
     logging.log(level=logging.DEBUG, msg=f"{os.getcwd()}")
     logging.log(level=logging.INFO, msg="Created the directory structure!")
+
+    global reader
+    reader = easyocr.Reader(["en"])
+
+    global coordinates_dict
+    coordinates_dict = {
+        "2": {
+            "heart_rate": [949, 58, 1082, 151],
+            "map": [1209, 158, 1350, 230],
+            "spo2": [942, 318, 1085, 423],
+            "rr": [929, 420, 948, 530],
+            "dbp": [1099, 159, 1178, 221],
+            "sbp": [946, 162, 1058, 217],
+        },
+        "3": {
+            "rr": [915, 532, 1247, 712],
+            "dbp": [303, 562, 536, 750],
+            "spo2": [937, 260, 1246, 445],
+            "map": [563, 599, 801, 770],
+            "heart_rate": [880, 41, 1240, 207],
+            "sbp": [70, 550, 276, 765],
+        },
+        "4": {
+            "map": [141, 378, 272, 447],
+            "heart_rate": [71, 80, 415, 242],
+            "spo2": [93, 469, 424, 605],
+            "sbp": [18, 310, 195, 405],
+            "rr": [303, 646, 452, 726],
+            "dbp": [257, 317, 442, 398],
+        },
+        "1": {
+            "map": [899, 581, 1108, 690],
+            "heart_rate": [0, 0, 281, 185],
+            "sbp": [681, 394, 955, 574],
+            "spo2": [7, 345, 358, 573],
+            "rr": [678, 48, 998, 249],
+            "dbp": [1058, 386, 1252, 581],
+        },
+    }
+
+    try:
+        global classifier_model
+        classifier_model = tf.keras.models.load_model(filepath="./classifier/model.h5")
+        logging.log(logging.INFO, "Loaded the classifier model")
+    except Exception as e:
+        logging.log(logging.ERROR, e)
+        logging.log(logging.ERROR, "Unable to load the classifier model")
+        exit(1)
 
     sleep(2)
 
@@ -73,6 +127,33 @@ def get_segmentation(img_path: str):
         return None
 
 
+def get_monitor_type(img):
+    """
+    This function returns the type of the image
+    Input: img: np.ndarray
+    Output: type: str
+    """
+
+    img = np.array(img).astype(dtype="float32") / 255.0
+    img = cv2.resize(img, (224, 224))
+    x = np.expand_dims(img, axis=0)
+    k = None
+    if classifier_model is not None:
+        preds = classifier_model.predict(x)
+        k = np.argmax(preds)
+
+    if k == 0:
+        return "1"
+    elif k == 1:
+        return "2"
+    elif k == 2:
+        return "3"
+    elif k == 3:
+        return "4"
+    else:
+        return None
+
+
 def main() -> None:
     img_queue = queue.Queue()
 
@@ -88,8 +169,19 @@ def main() -> None:
         if not img_queue.empty():
             img_path = img_queue.get()
             logging.log(level=logging.DEBUG, msg=f"{img_queue.qsize()}")
-            logging.log(level=logging.DEBUG, msg=f"Got the image path: {img_path}")
             segmentation = get_segmentation(img_path=img_path)
+
+            if segmentation is None or segmentation.shape[0] != 4:
+                logging.log(
+                    level=logging.WARNING,
+                    msg="segmentation is either None or segmentataion is not apt.",
+                )
+                continue
+
+            """
+            TODO: check if the segmentation length is strictly equal to 4 or not if not then skip for now and create alert...
+            """
+
             logging.log(
                 level=logging.INFO, msg="Got the segmentation mask of the image"
             )
@@ -100,6 +192,16 @@ def main() -> None:
                     image=img, input_array=segmentation
                 )
                 logging.log(level=logging.INFO, msg="Got the processed image")
+
+                monitor_type = get_monitor_type(processed_image)
+                print(monitor_type)
+                print(
+                    extract_information_from_image(
+                        img=processed_image,
+                        coordinates_dict=coordinates_dict[monitor_type],
+                        kwargs={"reader": reader},
+                    )
+                )
 
                 try:
                     if not os.path.exists("processed_images"):
---
+++ readme.md
@@ -0,0 +1,51 @@
+# Cloudphysician
+Read [this document](./static/project-proposal.pdf) for the motivation to make this project. 
+
+## Dependencies
+- **Python3**: Ensure that you have Python 3 installed on your system. You can download and install Python 3 from the official Python website: https://www.python.org.
+- **pip**: pip is the package installer for Python. It is usually installed by default when you install Python. However, make sure you have pip installed and it is up to date. You can check the version of pip by running the following command:
+  ```
+  pip --version
+  ```
+## Installation
+To install and use Cloudphysician, follow the steps given below:
+- Fork the Cloudphysician repository by clicking the "Fork" button at the top right corner of the repository page. This will create a copy of the repository under your GitHub account.
+- Clone the forked repository to your local machine:
+    ```
+    git clone https://github.com/{YOUR-USERNAME}/Cloudphysician
+    ```
+- Navigate to the project directory: 
+    ```
+    cd Cloudphysician
+    ```
+- Install the necessary Python packages by running the following command:
+  ```
+  pip install -r requirements.txt
+  ```
+(NOTE: It is recommended to install these requirements in a new python environment)
+
+## How to use?
+
+Follow the steps given below:
+
+
+
+## Output
+### Input Image
+![sample_image.jpeg](./static/sample_image.jpeg)
+
+### Result
+![results.jpeg](./static/results.jpeg)
+
+## Future Developement
+
+
+## Contributions
+Contributions to Cloudphysician are welcome! If you encounter any issues or have suggestions for improvements, please feel free to open an issue or submit a pull request on the GitHub repository.
+
+## Author
+[Abhishek Singh Kushwaha](https://github.com/ASK-03)
+
+[Kriti Gupta](https://github.com/Kriti1106)
+
+[Bhavik Shangari](https://github.com/BhavikShangari)
